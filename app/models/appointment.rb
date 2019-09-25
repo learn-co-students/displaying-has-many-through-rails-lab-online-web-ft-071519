bbe8f1bc128cee98aac2b9c38fa28b493caa3fa9
radville <laura.radville@gmail.com>
2019-09-25 16:10:56 -0400

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,12 +1,4 @@
 class Appointment < ApplicationRecord
     belongs_to :doctor
     belongs_to :patient
-
-    def doctor
-        Doctor.find(self.doctor_id)
-    end
-
-    def patient
-        Patient.find(self.patient_id)
-    end
 end
