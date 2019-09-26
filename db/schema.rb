0cf757e2394f4680e2e1599bc40844a266f908d6
Ahmed Abououf <aabuauf@yahoo.com>
2019-09-26 10:22:57 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190925233043) do
+ActiveRecord::Schema.define(version: 20190925232930) do
 
   create_table "appointments", force: :cascade do |t|
-    t.string   "appointment_datetime"
-    t.integer  "patient_id"
+    t.datetime "appointment_datetime"
     t.integer  "doctor_id"
+    t.integer  "patient_id"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
   end
