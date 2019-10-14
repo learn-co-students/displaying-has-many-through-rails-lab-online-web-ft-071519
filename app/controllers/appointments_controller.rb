e40a8d19a1835e7e8e410fce28dad75a389c582e
Steven Hunal <stevehunal@gmail.com>
2019-10-14 15:02:53 -0400

    Done.

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,19 @@
+class AppointmentsController < ApplicationController
+    def index 
+
+    end 
+    
+    def show 
+
+    end 
+    
+    def new 
+
+    end 
+    
+    def create 
+
+    end 
+
+    
+end
---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,17 @@
+class DoctorsController < ApplicationController
+    def index 
+
+    end 
+    
+    def show 
+
+    end 
+    
+    def new 
+
+    end 
+    
+    def create 
+
+    end 
+end
